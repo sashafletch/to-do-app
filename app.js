beda25456c661413832b1942ac25b31cbc5c94fe
Sasha <rashaafletcher@gmail.com>
2018-04-23 14:51:39 -0700

    dom part 2 assignment completed

--- app.js
+++ app.js
@@ -1,33 +1,53 @@
 function onReady() {
-  const toDos = [];
-  const addToDoForm = document.getElementByID('addToDoForm');
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
   function createNewToDo() {
-    const newToDoText = document.getElementByID('newToDoText');
-    if (!newToDoText.value) { return; }
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; };
+
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id.value
     });
+
     newToDoText.value = '';
+    id++;
 
     renderTheUI();
   }
 
   function renderTheUI() {
-    const toDoList = document.getElementByID('toDoList');
+    const toDoList = document.getElementById('toDoList');
 
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('button');
+
+      function deleteToDo(){
+        toDos = toDos.filter(item => {
+          return item !== toDo;
+      });
+    }
+
+    deleteButton.addEventListener('click', event => {
+      deleteToDo();
+      renderTheUI();
+    });
+
       checkbox.type = "checkbox";
 
       newLi.textContent = toDo.title;
+      deleteButton.textContent = 'Delete';
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
     });
 
   }
@@ -35,7 +55,10 @@ function onReady() {
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
+
   });
+
+
 
   renderTheUI();
 }
